f5bc4b7445af4373a604cba2143cafb9963e9407
Kevin Cui <krazycavin@gmail.com>
2020-03-29 15:33:43 +0200

    Loop 10s to get final translation

--- translate.js
+++ translate.js
@@ -7,7 +7,7 @@ program
     .name('./translate.js')
     .usage('[-p <path>] [-f <m_lang] [-t <lang>] <text>')
     .option('-p, --path <binary_path>', 'path to chrome/chromium binary\ndefault "/usr/bin/chromium"')
-    .option('-f, --from <lang_iso>', 'language ISO code of text to translate\ndefault "auto"\nthis option is not available for deepl')
+    .option('-f, --from <lang_iso>', 'language ISO code of text to translate\ndefault "auto"')
     .option('-t, --to <lang_iso>', 'language ISO code of target language\ndefault "en"')
     .option('-s, --service <service>', 'supported service: google, deepl\ndefault "goodle"');
 
@@ -24,7 +24,7 @@ const sName = (program.service === undefined) ? 'google' : program.service;
     const isheadless = true;
 
     if (sName == "deepl") {
-        var url = 'https://www.deepl.com/' + tLang + '/translator';
+        var url = 'https://www.deepl.com/translator#' + fLang + '/' + tLang + '/' + text;
         var inputSource = '.lmt__source_textarea';
         var outputTranslation= '.lmt__translations_as_text__text_btn';
     } else {
@@ -37,12 +37,20 @@ const sName = (program.service === undefined) ? 'google' : program.service;
     const page = await browser.newPage();
     await page.goto(url, {timeout: 30000, waitUntil: 'domcontentloaded'});
 
-    await page.waitForSelector(inputSource)
-    await page.$eval(inputSource, (el, value) => el.value = value, text);
-    page.keyboard.press(String.fromCharCode(13));
+    if (sName == 'google') {
+        await page.waitForSelector(inputSource)
+        await page.$eval(inputSource, (el, value) => el.value = value, text);
+    }
 
     await page.waitForSelector(outputTranslation);
-    const out = await page.evaluate((el) => { return document.querySelector(el).innerText }, outputTranslation);
+    for (var i = 0; i < 10; i++) {
+        var out = await page.evaluate((el) => { return document.querySelector(el).innerText; }, outputTranslation);
+        if (out !== "") {
+            break;
+        }
+        await page.waitFor(1000);
+    }
+
     console.log(out);
     await browser.close()
 })();
